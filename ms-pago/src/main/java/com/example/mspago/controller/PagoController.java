0ba71c2f03ca23f2e65cdfe4e439eadcfe58b759
jhanslarico11 <jhanslarico11@gmail.com>
2023-10-29 11:53:55 -0500

    pago

---
+++ ms-pago/src/main/java/com/example/mspago/controller/PagoController.java
@@ -0,0 +1,40 @@
+package com.example.mspago.controller;
+
+import com.example.mspago.entity.Pago;
+import com.example.mspago.service.PagoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/pagos")
+public class PagoController {
+    private final PagoService pagoService;
+
+    @Autowired
+    public PagoController(PagoService pagoService) {
+        this.pagoService = pagoService;
+    }
+
+    @GetMapping
+    public List<Pago> obtenerTodosLosPagos() {
+        return pagoService.obtenerTodosLosPagos();
+    }
+
+    @PostMapping
+    public Pago crearPago(@RequestBody Pago pago) {
+        return pagoService.crearPago(pago);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Pago> obtenerPagoPorId(@PathVariable Integer id) {
+        return pagoService.obtenerPagoPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void vaciarPago(@PathVariable Integer id) {
+        pagoService.vaciarPago(id);
+    }
+}
